4e8d27763ea869e0d34395ddc53e028f03ab7ea8
John Patrick Amata <johnamata@gmx.com>
2016-08-29 23:16:47 +0800

    main testing file

---
+++ Application/Main.java
@@ -0,0 +1,23 @@
+package project.pbl1;
+
+import java.io.StringReader;
+import java.util.List;
+import java.util.Scanner;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        String question;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a question:");
+        question = scanner.nextLine();
+
+    }
+}
+
+
+
